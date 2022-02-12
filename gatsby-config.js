db9ef0498d2197d0aa8d35bddf1328b154ddec97
Aaron J. Smith <onsmith@cs.unc.edu>
2022-02-11 21:22:24 -0500

    Install Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,5 +52,27 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+
+    // Google Analytics
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-120544595-1', // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ['/preview/**', '/do-not-track/me/too/'],
+          // Defaults to https://www.googletagmanager.com
+          origin: 'https://www.onsmith.com',
+        },
+      },
+    },
   ],
 };
